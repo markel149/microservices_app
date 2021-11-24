d381f5354ac70f58288abbdee3ee49edd41094e5
markel149 <markel.orallo@gmail.com>
2021-11-24 18:34:55 +0100

    ssl

--- machine/application/messaging_consumer.py
+++ machine/application/messaging_consumer.py
@@ -7,7 +7,7 @@ from application.machine import Machine
 from flask import request, jsonify, abort
 from werkzeug.exceptions import NotFound, InternalServerError, BadRequest, UnsupportedMediaType
 from .messaging_producer import send_message
-
+import ssl
 my_machine = Machine()
 
 class Consumer:
@@ -19,8 +19,14 @@ class Consumer:
         self.declare_connection()
 
     def declare_connection(self):
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='rabbitmq'))
+                # Define ssl context 
+        context = ssl.create_default_context(cafile="/app/application/certs/ca_certificate.pem")
+        context.load_cert_chain("/app/application/certs/client_certificate.pem","/app/application/certs/client_key.pem", password="bunnies")
+        ssl_options = pika.SSLOptions(context, "rabbitmq")
+        #conn_params = pika.ConnectionParameters(port=5671,ssl_options=ssl_options)
+    
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', ssl_options=ssl_options, port=5672))
         channel = connection.channel()
         channel.exchange_declare(exchange=self.exchange_name, exchange_type='topic')
 
